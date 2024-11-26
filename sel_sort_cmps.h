e9442ade6d78cbbd0e761f978dddd13510714efb
tbazarov <159284610+tbazarov@users.noreply.github.com>
2024-11-26 22:16:58 +0700

    Add files via upload

---
+++ sel_sort_cmps.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stddef.h>
+
+int compare_int(void *a, void *b);
+int compare_float(void *a, void *b);
+int compare_char(void *a, void *b);
+int compare_string(void *a, void *b);
