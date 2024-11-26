b43e0a19f157d3be8930dfecf37d941c9e374408
tbazarov <159284610+tbazarov@users.noreply.github.com>
2024-11-26 22:19:07 +0700

    Add files via upload

--- sel_sort_declars.h
+++ sel_sort_declars.h
@@ -1,7 +1,4 @@
+#pragma once
 #include <stddef.h>
 
-int compare_int(void *a, void *b);
-int compare_float(void *a, void *b);
-int compare_char(void *a, void *b);
-int compare_string(void *a, void *b);
 void selection_sort(void *array, int n, size_t size, int (*cmp)(void*, void*));
